beea870ebdccdb8bd559f4c0951611e50e27651e
Justin Coyne <justin@curationexperts.com>
2015-02-11 14:13:08 -0800

    Add more association tests

--- spec/models/hydra/works/generic_file_spec.rb
+++ spec/models/hydra/works/generic_file_spec.rb
@@ -4,10 +4,24 @@ describe Hydra::Works::GenericFile do
 
   describe "associations" do
     let(:work) { Hydra::Works::GenericWork.new }
-    subject { Hydra::Works::GenericFile.new(work: work) }
+    context "base model" do
+      subject { Hydra::Works::GenericFile.new(work: work) }
 
-    it "should belong to works" do
-      expect(subject.work).to eq work
+      it "should belong to works" do
+        expect(subject.work).to eq work
+      end
+    end
+
+    context "sub-class" do
+      before do
+        class TestFile < Hydra::Works::GenericFile
+        end
+      end
+      subject { TestFile.new(work: work) }
+
+      it "should belong to works" do
+        expect(subject.work).to eq work
+      end
     end
   end
 end
--- spec/models/hydra/works/generic_work_spec.rb
+++ spec/models/hydra/works/generic_work_spec.rb
@@ -10,10 +10,25 @@ describe Hydra::Works::GenericWork do
 
   describe "associations" do
     let(:file) { Hydra::Works::GenericFile.new }
-    subject { Hydra::Works::GenericWork.create(title: ['test'], generic_files: [file]) }
+    context "base model" do
+      subject { Hydra::Works::GenericWork.create(title: ['test'], generic_files: [file]) }
 
-    it "should have many generic files" do
-      expect(subject.generic_files).to eq [file]
+      it "should have many generic files" do
+        expect(subject.generic_files).to eq [file]
+      end
+    end
+
+    context "sub-class" do
+      before do
+        class TestWork < Hydra::Works::GenericWork
+        end
+      end
+
+      subject { TestWork.create(title: ['test'], generic_files: [file]) }
+
+      it "should have many generic files" do
+        expect(subject.generic_files).to eq [file]
+      end
     end
   end
 end
