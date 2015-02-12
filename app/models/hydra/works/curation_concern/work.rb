9bcbd72d6725d1d09bab0d9154c3b09f2ee94ab9
Justin Coyne <justin@curationexperts.com>
2015-02-11 16:18:05 -0800

    Remove to_solr

--- app/models/hydra/works/curation_concern/work.rb
+++ app/models/hydra/works/curation_concern/work.rb
@@ -3,12 +3,6 @@ module Hydra::Works
     module Work
       extend ActiveSupport::Concern
       include WithGenericFiles
-
-      def to_solr(solr_doc={})
-        super.tap do |solr_doc|
-          Solrizer.set_field(solr_doc, 'generic_type', 'Work', :facetable)
-        end
-      end
     end
   end
 end
