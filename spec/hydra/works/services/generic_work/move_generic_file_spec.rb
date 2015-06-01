544127d0a302fbc5c2afdb6d8f5efb329924f36c
Matt Zumwalt <matt@curationexperts.com>
2015-06-01 10:11:56 -0500

    Fixes bug from last merge (somehow we missed changing this file)

--- spec/hydra/works/services/generic_work/move_generic_file_spec.rb
+++ spec/hydra/works/services/generic_work/move_generic_file_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe Hydra::Works::MoveGenericFileToGenericWork do
-  let(:source_work) { Hydra::Works::GenericWork.create }
-  let(:target_work) { Hydra::Works::GenericWork.create }
-  let(:file1) { Hydra::Works::GenericFile.create }
-  let(:file2) { Hydra::Works::GenericFile.create }
+  let(:source_work) { Hydra::Works::GenericWork::Base.create }
+  let(:target_work) { Hydra::Works::GenericWork::Base.create }
+  let(:file1) { Hydra::Works::GenericFile::Base.create }
+  let(:file2) { Hydra::Works::GenericFile::Base.create }
   context "move generic file" do 
     before do 
       Hydra::Works::AddGenericFileToGenericWork.call(source_work, file1)
