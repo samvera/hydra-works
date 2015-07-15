aa66ee0e6d8880e3d9f5fc090930a74a139f01f5
Kevin Reiss <kevin.reiss@gmail.com>
2015-07-15 02:11:46 -0400

    Test coverage for Hydra::Works::GenericFile::Derivatives.
    Remove datastream from makes_derivatives.
    
    update to new signature
    
    move tests over from curation_concerns.
    
    get persist_derivatives pending specs to pass
    
    update readme

--- lib/hydra/works/models/concerns/generic_file/derivatives.rb
+++ lib/hydra/works/models/concerns/generic_file/derivatives.rb
@@ -5,27 +5,23 @@ module Hydra::Works::GenericFile
     included do
       include Hydra::Derivatives
 
+      # Sets output_file_service to PersistDerivative instead of default Hydra::Derivatives::PersistBasicContainedOutputFileService
+      Hydra::Derivatives.output_file_service = Hydra::Works::PersistDerivative
+
       # This was taken directly from Sufia's GenericFile::Derivatives and modified to exclude any processing that modified the original file
       makes_derivatives do |obj|
-        case obj.mime_type
+        case obj.original_file.mime_type
         when *pdf_mime_types
-          obj.transform_file :original_file, { thumbnail: { format: 'jpg', size: '338x493', datastream: 'thumbnail' } }, processor: :work
+          obj.transform_file :original_file, thumbnail: { format: 'jpg', size: '338x493' }
         when *office_document_mime_types
-          obj.transform_file :original_file, { thumbnail: { format: 'jpg', size: '200x150>', datastream: 'thumbnail' } }, processor: :document
+          obj.transform_file :original_file, { thumbnail: { format: 'jpg', size: '200x150>' } }, processor: :document
         when *video_mime_types
-          obj.transform_file :original_file, { thumbnail: { format: 'jpg', datastream: 'thumbnail' } }, processor: :video
+          obj.transform_file :original_file, { thumbnail: { format: 'jpg' } }, processor: :video
         when *image_mime_types
-          obj.transform_file :original_file, { thumbnail: { format: 'jpg', size: '200x150>', datastream: 'thumbnail' } }, processor: :work
+          obj.transform_file :original_file, thumbnail: { format: 'jpg', size: '200x150>' }
         end
       end
 
-    end
-
-    # This bit was not in Sufia, but needs to be here for derivatives to work
-    # TODO: delegate mime type somewhere else?
-    def mime_type
-      return nil unless self.original_file
-      self.original_file.mime_type
     end
 
   end
--- lib/hydra/works/services/generic_file/persist_derivative.rb
+++ lib/hydra/works/services/generic_file/persist_derivative.rb
@@ -9,13 +9,13 @@ module Hydra::Works
     # The purpose of this Service is for use as an alternative to the default Hydra::Derivatives::PersistOutputFileService.  It's necessary because the default behavior in Hydra::Derivatives assumes that you're using LDP Basic Containment. Hydra::Works::GenericFiles use IndirectContainment.  This Service handles that case.
     # 
     # @param [Hydra::Works::GenericFile::Base] object the file will be added to
-    # @param [#read] file the dervivative filestream optionally responds to :mime_type
-    # @option opts [RDF::URI or String] type URI for the RDF.type that identifies the file's role within the generic_file
+    # @param [#read or String] file the derivative filestream optionally responds to :mime_type
+    # @param [String] extract file type symbol (e.g. :thumbnail) from Hydra::Derivatives created destination_name
     # @option opts [Boolean] update_existing when set to false, always adds a new file.  When set to true, performs a create_or_update
     # @option opts [Boolean] versioning whether to create new version entries
 
-    def self.call(object, file, opts={})
-      type = opts.fetch(:type, ::RDF::URI("http://pcdm.org/use#ServiceFile"))
+    def self.call(object, file, destination_name, opts={})
+      type = destination_name.gsub(/^original_file_/, '').to_sym
       update_existing = opts.fetch(:update_existing, true)
       versioning = opts.fetch(:versioning, true)
       Hydra::Works::AddFileToGenericFile.call(object, file, type, update_existing: update_existing, versioning: versioning)
