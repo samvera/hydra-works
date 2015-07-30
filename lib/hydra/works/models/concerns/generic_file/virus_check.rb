508608808cec015372811a91aa5ad667f35a212c
Esmé Cowles <escowles@ucsd.edu>
2015-07-30 09:54:33 -0400

    Migrating virus check functionality from CurationConcerns

---
+++ lib/hydra/works/models/concerns/generic_file/virus_check.rb
@@ -0,0 +1,48 @@
+module Hydra::Works::GenericFile
+  module VirusCheck
+    extend ActiveSupport::Concern
+    
+    included do
+      validate :detect_viruses
+    end
+
+    # Default behavior is to raise a validation error and halt the save if a virus is found
+    def detect_viruses
+      return unless original_file && original_file.new_record?
+
+      path = original_file.is_a?(String) ? original_file : local_path_for_file(original_file)
+      unless defined?(ClamAV)
+        warn "Virus checking disabled, #{path} not checked"
+        return
+      end
+
+      scan_result = ClamAV.instance.scanfile(path)
+      if scan_result == 0
+        true
+      else
+        virus_message = "A virus was found in #{path}: #{scan_result}"
+        warn(virus_message)
+        errors.add(:base, virus_message)
+        false
+      end
+    end
+
+    private
+
+    # Returns a path for reading the content of +file+
+    # @param [File] file object to retrieve a path for
+    def local_path_for_file(file)
+      if file.respond_to?(:path)
+        file.path
+      else
+        Tempfile.open('') do |t|
+          t.binmode
+          t.write(file)
+          t.close
+          t.path
+        end
+      end
+    end
+
+  end
+end
