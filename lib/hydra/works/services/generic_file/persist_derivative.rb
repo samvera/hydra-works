2202f8c48ddc6091a5029591d57e7fcc2c925f52
Justin Coyne <jcoyne@justincoyne.com>
2015-09-29 16:32:48 -0500

    Use hydra-derivatives 3.0.0.alpha

--- lib/hydra/works/services/generic_file/persist_derivative.rb
+++ lib/hydra/works/services/generic_file/persist_derivative.rb
@@ -8,13 +8,28 @@ module Hydra::Works
     # The purpose of this Service is for use as an alternative to the default Hydra::Derivatives::PersistOutputFileService.  It's necessary because the default behavior in Hydra::Derivatives assumes that you're using LDP Basic Containment. Hydra::Works::GenericFiles use IndirectContainment.  This Service handles that case.
     # This service will always update existing and does not do versioning of persisted files.
     #
-    # @param [Hydra::Works::GenericFile::Base] object the file will be added to
-    # @param [Hydra::Derivatives::IoDecorator] file the derivative filestream
-    # @param [String] extract file type symbol (e.g. :thumbnail) from Hydra::Derivatives created destination_name
-
-    def self.call(object, file, destination_name)
-      type = destination_name.gsub(/^original_file_/, '').to_sym
+    # @param [#read] stream the derivative filestream
+    # @param [Hash] directives
+    # @option directives [GenericFile] :object the GenericFile object to attach to
+    # @option directives [Symbol] :label the type of derivative
+    # extract file type symbol (e.g. :thumbnail) from Hydra::Derivatives created destination_name
+    def self.call(stream, directives)
+      file = Hydra::Derivatives::IoDecorator.new(stream)
+      file.mime_type = new_mime_type(directives.fetch(:format))
+      object = directives.fetch(:object)
+      type = directives.fetch(:label)
       Hydra::Works::AddFileToGenericFile.call(object, file, type, update_existing: true, versioning: false)
+    end
+
+    def self.new_mime_type(format)
+      case format
+      when 'mp4'
+        'video/mp4' # default is application/mp4
+      when 'webm'
+        'video/webm' # default is audio/webm
+      else
+        MIME::Types.type_for(format).first.to_s
+      end
     end
   end
 end
