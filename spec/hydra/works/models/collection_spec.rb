2350998238829a8b5c3f0e1bc2dba92002a33280
Justin Coyne <jcoyne@justincoyne.com>
2015-12-07 11:31:26 -0600

    Refactor tests

--- spec/hydra/works/models/collection_spec.rb
+++ spec/hydra/works/models/collection_spec.rb
@@ -1,68 +1,87 @@
 require 'spec_helper'
 
 describe Hydra::Works::Collection do
-  subject { described_class.new }
+  let(:collection) { described_class.new }
 
   let(:collection1) { described_class.new }
-  let(:collection2) { described_class.new }
-  let(:collection3) { described_class.new }
-  let(:collection4) { described_class.new }
-
   let(:generic_work1) { Hydra::Works::GenericWork.new }
-  let(:generic_work2) { Hydra::Works::GenericWork.new }
-  let(:generic_work3) { Hydra::Works::GenericWork.new }
-  let(:generic_work4) { Hydra::Works::GenericWork.new }
 
   describe '#collections' do
     it 'returns empty array when only works are aggregated' do
-      subject.ordered_members << generic_work1
-      subject.ordered_members << generic_work2
-      expect(subject.collections).to eq []
+      collection.ordered_members << generic_work1
+      expect(collection.collections).to eq []
     end
 
     context 'with other collections & works' do
+      let(:collection2) { described_class.new }
       before do
-        subject.ordered_members << collection1
-        subject.ordered_members << collection2
-        subject.ordered_members << generic_work1
-        subject.ordered_members << generic_work2
+        collection.ordered_members << collection1
+        collection.ordered_members << collection2
+        collection.ordered_members << generic_work1
       end
 
       it 'returns only collections' do
-        expect(subject.ordered_collections).to eq [collection1, collection2]
+        expect(collection.ordered_collections).to eq [collection1, collection2]
       end
     end
   end
 
   describe '#works' do
-    it 'returns empty array when only collections are aggregated' do
-      subject.ordered_members << collection1
-      subject.ordered_members << collection2
-      expect(subject.ordered_works). to eq []
+    subject { collection.works }
+    context "when only collections are aggregated" do
+      it 'returns empty array when only collections are aggregated' do
+        collection.ordered_members << collection1
+        expect(subject).to eq []
+      end
     end
 
     context 'with collections and generic works' do
+      let(:generic_work2) { Hydra::Works::GenericWork.new }
       before do
-        subject.ordered_members << collection1
-        subject.ordered_members << collection2
-        subject.ordered_members << generic_work1
-        subject.ordered_members << generic_work2
+        collection.ordered_members << collection1
+        collection.ordered_members << generic_work1
+        collection.ordered_members << generic_work2
       end
 
       it 'returns only generic works' do
-        expect(subject.ordered_works). to eq [generic_work1, generic_work2]
+        expect(subject).to eq [generic_work1, generic_work2]
+      end
+    end
+  end
+
+  describe '#ordered_works' do
+    subject { collection.ordered_works }
+    context "when only collections are aggregated" do
+      it 'returns empty array when only collections are aggregated' do
+        collection.ordered_members << collection1
+        expect(subject).to eq []
+      end
+    end
+
+    context 'with collections and generic works' do
+      let(:generic_work2) { Hydra::Works::GenericWork.new }
+      before do
+        collection.ordered_members << collection1
+        collection.ordered_members << generic_work1
+        collection.ordered_members << generic_work2
+      end
+
+      it 'returns only generic works' do
+        expect(subject).to eq [generic_work1, generic_work2]
       end
     end
   end
 
   describe "#ordered_work_ids" do
+    subject { collection.ordered_work_ids }
     it "returns IDs of ordered works" do
-      subject.ordered_members << generic_work1
-      expect(subject.ordered_work_ids).to eq [generic_work1.id]
+      collection.ordered_members << generic_work1
+      expect(subject).to eq [generic_work1.id]
     end
   end
 
-  describe 'Related objects' do
+  describe '#related_objects' do
+    subject { collection.related_objects }
     let(:object) { Hydra::PCDM::Object.new }
     let(:collection) { described_class.new }
 
@@ -70,22 +89,16 @@ describe Hydra::Works::Collection do
       collection.related_objects = [object]
     end
 
-    it 'persists' do
-      expect(collection.related_objects).to eq [object]
-    end
+    it { is_expected.to eq [object] }
   end
 
-  describe 'have parent collection accessors' do
+  describe "#in_collections" do
     before do
-      collection1.ordered_members << collection2
+      collection1.ordered_members << collection
       collection1.save
     end
 
-    it 'has parents' do
-      expect(collection2.member_of).to eq [collection1]
-    end
-    it 'has a parent collection' do
-      expect(collection2.in_collections).to eq [collection1]
-    end
+    subject { collection.in_collections }
+    it { is_expected.to eq [collection1] }
   end
 end
--- spec/hydra/works/models/file_set_spec.rb
+++ spec/hydra/works/models/file_set_spec.rb
@@ -1,48 +1,44 @@
 require 'spec_helper'
 
 describe Hydra::Works::FileSet do
-  let(:generic_file1) { described_class.new }
+  let(:file_set) { described_class.new }
 
   describe 'Related objects' do
     let(:object1) { Hydra::PCDM::Object.new }
 
     before do
-      generic_file1.related_objects = [object1]
+      file_set.related_objects = [object1]
     end
 
     it 'persists' do
-      expect(generic_file1.related_objects).to eq [object1]
+      expect(file_set.related_objects).to eq [object1]
     end
   end
 
   describe '#files' do
-    let(:object) { described_class.create }
-    let(:file1) { object.files.build }
-    let(:file2) { object.files.build }
+    let(:file1) { file_set.files.build }
+    let(:file2) { file_set.files.build }
 
     before do
+      file_set.save!
       file1.content = "I'm a file"
       file2.content = 'I am too'
-      object.save!
+      file_set.save!
     end
 
-    subject { described_class.find(object.id).files }
+    subject { described_class.find(file_set.id).files }
 
     it { is_expected.to eq [file1, file2] }
   end
 
-  describe 'parent work accessors' do
-    let(:generic_work1) { Hydra::Works::GenericWork.create }
+  describe '#in_works' do
+    subject { file_set.in_works }
+    let(:generic_work) { Hydra::Works::GenericWork.create }
     before do
-      generic_work1.ordered_members << generic_file1
-      generic_work1.save
+      generic_work.ordered_members << file_set
+      generic_work.save
     end
 
-    it 'has parents' do
-      expect(generic_file1.member_of).to eq [generic_work1]
-    end
-    it 'has a parent work' do
-      expect(generic_file1.in_works).to eq [generic_work1]
-    end
+    it { is_expected.to eq [generic_work] }
   end
 end
