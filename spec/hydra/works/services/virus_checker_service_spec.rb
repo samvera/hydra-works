0718f47f81b38a975e916c0c90cd52508703dfc8
Adam Wead <amsterdamos@gmail.com>
2016-05-16 12:42:46 -0400

    Use our own VirusScanner, fixes #296

--- spec/hydra/works/services/virus_checker_service_spec.rb
+++ spec/hydra/works/services/virus_checker_service_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Hydra::Works::VirusCheckerService do
-  let(:system_virus_scanner) { double(call: nil) }
+  let(:system_virus_scanner) { double }
   let(:file) { Hydra::PCDM::File.new { |f| f.content = File.new(File.join(fixture_path, 'sample-file.pdf')) } }
   let(:virus_checker) { described_class.new(file, system_virus_scanner) }
 
@@ -14,27 +14,17 @@ describe Hydra::Works::VirusCheckerService do
     end
   end
 
-  context 'with a system virus scanner that did not run' do
-    let(:virus_checker) { described_class.new(file) }
-    it 'will return false and set a system warning' do
-      expect(defined?(ClamAV)).to eq(nil) # A bit of a sanity test to make sure the default behaves
-      allow(file).to receive(:path).and_return('/tmp/file.pdf')
-      expect(virus_checker).to receive(:warning).with(kind_of(String))
-      expect(virus_checker.file_has_virus?).to eq(true)
-    end
-  end
-
   context 'with an infected file' do
     context 'that responds to :path' do
       it 'will return false' do
-        expect(system_virus_scanner).to receive(:call).with('/tmp/file.pdf').and_return(1)
+        expect(system_virus_scanner).to receive(:infected?).with('/tmp/file.pdf').and_return(true)
         allow(file).to receive(:path).and_return('/tmp/file.pdf')
         expect(virus_checker.file_has_virus?).to eq(true)
       end
     end
     context 'that does not respond to :path' do
       it 'will return false' do
-        expect(system_virus_scanner).to receive(:call).with(kind_of(String)).and_return(1)
+        expect(system_virus_scanner).to receive(:infected?).with(kind_of(String)).and_return(true)
         allow(file).to receive(:respond_to?).and_call_original
         allow(file).to receive(:respond_to?).with(:path).and_return(false)
         expect(virus_checker.file_has_virus?).to eq(true)
@@ -45,50 +35,17 @@ describe Hydra::Works::VirusCheckerService do
   context 'with a clean file' do
     context 'that responds to :path' do
       it 'will return true' do
-        expect(system_virus_scanner).to receive(:call).with('/tmp/file.pdf').and_return(0)
+        expect(system_virus_scanner).to receive(:infected?).with('/tmp/file.pdf').and_return(false)
         allow(file).to receive(:path).and_return('/tmp/file.pdf')
         expect(virus_checker.file_has_virus?).to eq(false)
       end
     end
     context 'that does not respond to :path' do
       it 'will return true' do
-        expect(system_virus_scanner).to receive(:call).with(kind_of(String)).and_return(0)
+        expect(system_virus_scanner).to receive(:infected?).with(kind_of(String)).and_return(false)
         allow(file).to receive(:respond_to?).and_call_original
         allow(file).to receive(:respond_to?).with(:path).and_return(false)
         expect(virus_checker.file_has_virus?).to eq(false)
-      end
-    end
-  end
-
-  context '#default_system_virus_scanner' do
-    let(:virus_checker) { described_class.new(file) }
-    let(:system_virus_scanner) { virus_checker.send(:default_system_virus_scanner) }
-    it 'is callable' do
-      expect(system_virus_scanner).to respond_to(:call)
-    end
-    context 'when called and ClamAV is NOT defined' do
-      it 'will warn and return :no_anti_virus_was_run if ClamAV is not defined' do
-        expect(system_virus_scanner.call('/tmp/path')).to eq(:no_anti_virus_was_run)
-      end
-    end
-    context 'when called and ClamAV is defined' do
-      before do
-        class ClamAV
-          def self.instance
-            @instance ||= ClamAV.new
-          end
-
-          def scanfile(path)
-            puts "scanfile: #{path}"
-          end
-        end
-      end
-      after do
-        Object.send(:remove_const, :ClamAV)
-      end
-      it "will call the Clam's scanfile" do
-        expect(ClamAV.instance).to receive(:scanfile).with('/tmp/path')
-        system_virus_scanner.call('/tmp/path')
       end
     end
   end
