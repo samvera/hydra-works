68ffcda8025e76699f0f2c2bf1f42ee861579d34
Carolyn Cole <cam156@psu.edu>
2017-01-18 11:26:43 -0500

    Changing how the file is retrieved for virus checking  If the content has a path just use that  If the file is not saved to fedora use the content  If the file is save use the datastream

--- spec/hydra/works/services/virus_checker_service_spec.rb
+++ spec/hydra/works/services/virus_checker_service_spec.rb
@@ -5,9 +5,11 @@ describe Hydra::Works::VirusCheckerService do
   let(:file) { Hydra::PCDM::File.new }
   let(:virus_checker) { described_class.new(file, system_virus_scanner) }
   let(:datastream) { instance_double "ActiveFedora::File::Streaming::FileBody" }
+  let(:content) { instance_double "File" }
 
   before do
     allow(file).to receive(:stream).and_return(datastream)
+    allow(file).to receive(:content).and_return(content)
     allow(datastream).to receive(:each)
   end
 
@@ -22,14 +24,15 @@ describe Hydra::Works::VirusCheckerService do
 
   context 'with an infected file' do
     context 'that responds to :path' do
-      it 'will return false' do
+      it 'will return true' do
         expect(system_virus_scanner).to receive(:infected?).with('/tmp/file.pdf').and_return(true)
         allow(file).to receive(:path).and_return('/tmp/file.pdf')
         expect(virus_checker.file_has_virus?).to eq(true)
       end
     end
     context 'that does not respond to :path' do
-      it 'will return false' do
+      let(:content) { instance_double "File", read: ["abc123", nil], rewind: true }
+      it 'will return true' do
         expect(system_virus_scanner).to receive(:infected?).with(kind_of(String)).and_return(true)
         allow(file).to receive(:respond_to?).and_call_original
         allow(file).to receive(:respond_to?).with(:path).and_return(false)
@@ -38,17 +41,47 @@ describe Hydra::Works::VirusCheckerService do
     end
   end
 
-  context 'with a clean file' do
+  context 'with a clean unsaved file' do
     context 'that responds to :path' do
-      it 'will return true' do
+      it 'will return false' do
         expect(system_virus_scanner).to receive(:infected?).with('/tmp/file.pdf').and_return(false)
         allow(file).to receive(:path).and_return('/tmp/file.pdf')
         expect(virus_checker.file_has_virus?).to eq(false)
       end
     end
+
+    context "that does not respond to path" do
+      before do
+        allow(file).to receive(:respond_to?).with(:path).and_return(false)
+      end
+      context 'that the content responds to path' do
+        let(:content) { instance_double "File", read: ["abc123", nil], rewind: true, path: "abc123" }
+
+        it 'will return false' do
+          expect(system_virus_scanner).to receive(:infected?).with(kind_of(String)).and_return(false)
+          expect(content).not_to receive(:read)
+          expect(virus_checker.file_has_virus?).to eq(false)
+        end
+      end
+      context "that the content does not respond to path" do
+        let(:content) { instance_double "File", read: ["abc123", nil], rewind: true }
+        it 'will return false' do
+          expect(system_virus_scanner).to receive(:infected?).with(kind_of(String)).and_return(false)
+          expect(file).not_to receive(:stream)
+          expect(virus_checker.file_has_virus?).to eq(false)
+        end
+      end
+    end
+  end
+
+  context "with a clean saved file that does not respond to :path" do
+    before do
+      allow(file).to receive(:new_record?).and_return(false)
+    end
     context 'that does not respond to :path' do
-      it 'will return true' do
+      it 'will return false' do
         expect(system_virus_scanner).to receive(:infected?).with(kind_of(String)).and_return(false)
+        expect(content).not_to receive(:read)
         allow(file).to receive(:respond_to?).and_call_original
         allow(file).to receive(:respond_to?).with(:path).and_return(false)
         expect(virus_checker.file_has_virus?).to eq(false)
